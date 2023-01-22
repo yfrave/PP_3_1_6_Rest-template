2f559b1b49c0fa4aef6386cd9ae278c30e8a6de0
yfrave <fanta008@mail.ru>
2023-01-22 18:11:34 +0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        POST http://94.198.50.185:7081/api/users
+//        Content-Type: application/json
+//        Cookie: {{my_cookie}}
+//
+//        {
+//            "id": 3,
+//                "name": "James",
+//                "lastName": "Brown",
+//                "age": 23
+//        }
+//###
+//
+//        PUT http://94.198.50.185:7081/api/users
+//        Content-Type: application/json
+//        Cookie: {{my_cookie}}
+//
+//        {
+//            "id": 3,
+//                "name": "Thomas",
+//                "lastName": "Shelby",
+//                "age": 50
+//        }
+//
+//###
+//
+//        DELETE http://94.198.50.185:7081/api/users/3
+//        Accept: application/json
+//        Cookie: {{my_cookie}}
+    }
+}
